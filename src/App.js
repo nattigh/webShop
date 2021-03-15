08ea8c792ad5467d3bea02ed4c628ea955bef04f
nattigh <n_atti11@yahoo.com>
2021-03-15 11:51:29 +0200

    App.js basic structure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <>
       <div>Ez lesz a header</div>
       <div>Itt lsznek a termekek</div>
+      <div>kell kulon oldal a termek leirasra</div>
       <div>Ez lesz a footer</div>
     </>
   );
