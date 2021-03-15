da67af66b0fbadaf5fa212978f1a2ecbde0492e4
nattigh <n_atti11@yahoo.com>
2021-03-15 10:59:52 +0200

    App.js basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import React from "react";
 function App() {
-  return <div>Ez</div>;
+  return (
+    <>
+      <div>Ez lesz a header</div>
+      <div>Itt lsznek a termekek</div>
+      <div>Ez lesz a footer</div>
+    </>
+  );
 }
 
 export default App;
