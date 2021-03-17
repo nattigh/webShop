370ddf6f6eb43c3fb914be8dcebdaf8c9fda0e28
nattigh <n_atti11@yahoo.com>
2021-03-17 13:59:46 +0200

    Main page final design

--- src/App.js
+++ src/App.js
@@ -21,71 +21,73 @@ function App() {
       <div className="content">
         <div className="products">
           <div className="product">
-            <h3>React book</h3>
             <img
               className="productImg"
               src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
               alt=""
             />
+            <h3>React book</h3>
             <p>25$</p>
-            <button>Add to cart</button>
           </div>
           <div className="product">
-            <h3>React book</h3>
             <img
               className="productImg"
-              src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
+              src="https://www.reiss.com/media/product/518/136/-mens-gardener-ls-varsity-strip-in-navy-blue-3.jpg?format=jpeg&auto=webp&quality=85&width=250&height=287&fit=bounds"
               alt=""
             />
+            <h3>React book</h3>
             <p>25$</p>
-            <button>Add to cart</button>
           </div>
           <div className="product">
-            <h3>React book</h3>
             <img
               className="productImg"
-              src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
+              src="https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
               alt=""
             />
+            <h3>React book</h3>
             <p>25$</p>
-            <button>Add to cart</button>
           </div>
           <div className="product">
-            <h3>React book</h3>
             <img
               className="productImg"
               src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
               alt=""
             />
+            <h3>React book</h3>
             <p>25$</p>
-            <button>Add to cart</button>
           </div>
           <div className="product">
-            <h3>React book</h3>
             <img
               className="productImg"
               src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
               alt=""
             />
+            <h3>React book</h3>
             <p>25$</p>
-            <button>Add to cart</button>
           </div>
-
           <div className="product">
-            <h3>React book</h3>
             <img
               className="productImg"
               src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
               alt=""
             />
+            <h3>React book</h3>
             <p>25$</p>
-            <button>Add to cart</button>
+          </div>
+          <div className="product">
+            <img
+              className="productImg"
+              src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
+              alt=""
+            />
+            <h3>React book</h3>
+            <p>25$</p>
           </div>
         </div>
       </div>
       {/*---*/}
       <footer>
-        <p>React js project for learning purposes only</p>
+        <p>This site does not offer any real products or services.</p>
       </footer>
     </>
   );
