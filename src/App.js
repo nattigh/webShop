38c640ba5cf35a69a5b43305649f862e6f1d2e6b
nattigh <n_atti11@yahoo.com>
2021-03-18 16:05:02 +0200

    Main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { products } from "./products";
 import "./index.css";
 //import "./normalize.css";
 function App() {
+  products.forEach((prod) => console.log(prod.name));
   return (
     <>
       {/*header*/}
@@ -20,69 +22,13 @@ function App() {
       {/*Products*/}
       <div className="content">
         <div className="products">
-          <div className="product">
-            <img
-              className="productImg"
-              src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
-              alt=""
-            />
-            <h3>React book</h3>
-            <p>25$</p>
-          </div>
-          <div className="product">
-            <img
-              className="productImg"
-              src="https://www.reiss.com/media/product/518/136/-mens-gardener-ls-varsity-strip-in-navy-blue-3.jpg?format=jpeg&auto=webp&quality=85&width=250&height=287&fit=bounds"
-              alt=""
-            />
-            <h3>React book</h3>
-            <p>25$</p>
-          </div>
-          <div className="product">
-            <img
-              className="productImg"
-              src="https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-              alt=""
-            />
-            <h3>React book</h3>
-            <p>25$</p>
-          </div>
-          <div className="product">
-            <img
-              className="productImg"
-              src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
-              alt=""
-            />
-            <h3>React book</h3>
-            <p>25$</p>
-          </div>
-          <div className="product">
-            <img
-              className="productImg"
-              src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
-              alt=""
-            />
-            <h3>React book</h3>
-            <p>25$</p>
-          </div>
-          <div className="product">
-            <img
-              className="productImg"
-              src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
-              alt=""
-            />
-            <h3>React book</h3>
-            <p>25$</p>
-          </div>
-          <div className="product">
-            <img
-              className="productImg"
-              src="https://images-na.ssl-images-amazon.com/images/I/4181vLK8kpL._SX331_BO1,204,203,200_.jpg"
-              alt=""
-            />
-            <h3>React book</h3>
-            <p>25$</p>
-          </div>
+          {products.map((prod) => (
+            <div className="product">
+              <img className="productImg" src={prod.image} alt={prod.name} />
+              <p>{prod.name}</p>
+              <p>{`${prod.price} £`}</p>
+            </div>
+          ))}
         </div>
       </div>
       {/*---*/}
